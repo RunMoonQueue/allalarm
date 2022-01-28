87ca9969eac602e18f8f2d1170d9ffec1254a190
LAPTOP-6LQBHRCU\skyli <skylife927@naver.com>
2022-01-28 09:02:40 +0900

    second ui

--- app/src/main/java/com/moon/allalarm/EditActivity.kt
+++ app/src/main/java/com/moon/allalarm/EditActivity.kt
@@ -1,32 +1,29 @@
 package com.moon.allalarm
 
-import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
 import com.moon.allalarm.databinding.ActivityEditBinding
 
 class EditActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityEditBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityEditBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        initView()
-    }
-
-
-    private fun initView() {
-        binding.plan.setOnClickListener {
-            binding.planLayout.visibility = View.VISIBLE
+        binding.option.setOnClickListener {
+            Log.i("MQ!", "option")
+            binding.optionLayout.visibility = View.VISIBLE
             binding.vibrationLayout.visibility = View.GONE
         }
-
         binding.vibration.setOnClickListener {
-            binding.planLayout.visibility = View.GONE
+            Log.i("MQ!", "vibration")
             binding.vibrationLayout.visibility = View.VISIBLE
+            binding.optionLayout.visibility = View.GONE
         }
     }
 }
